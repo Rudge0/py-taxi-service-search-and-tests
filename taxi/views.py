b42188bbd9d6cc0222f9dbc439567533568657d6
Maksym Pariichuk <maksim.pariychuk07@gmail.com>
2026-02-28 22:33:09 +0100

    Solution 2

--- taxi/views.py
+++ taxi/views.py
@@ -145,7 +145,9 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
         queryset = Driver.objects.all()
         form = DriverSearchForm(self.request.GET)
         if form.is_valid():
-            return queryset.filter(username__icontains=form.cleaned_data["username"])
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
 
         return queryset
 
